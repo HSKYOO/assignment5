035838c823c5e081b8624eb2d1589ee46850bd70
HSKYOO <hyunseok888@gmail.com>
2025-11-19 20:20:34 +0900

    add logic but need to add hook to develop

--- src/components/CreateModal.js
+++ src/components/CreateModal.js
@@ -1,7 +1,34 @@
 import React from "react";
 
-function CreateModla() {
+function CreateModal() {
     return (
-        <></>
+        <div className="modal fade" id="CreateModal" tabindex="-1">
+        <div className="modal-dialog">
+            <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title">새 학생 추가</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                <div className="modal-body">
+                    <div className="mb-3">
+                        <label className="form-label">Name</label>
+                        <input type="text" className="form-control" id="add_name"/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">age</label>
+                        <input type="number" className="form-control" id="add_age"/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Score</label>
+                        <input type="number" className="form-control" id="add_score"/>
+                    </div>
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">닫기</button>  
+                    <button type="button" className="btn btn-primary" id="btn_save_add">저장하기</button>
+                </div>
+            </div>
+        </div>
+    </div>
     );
 }
--- src/components/UpdateModal.js
+++ src/components/UpdateModal.js
@@ -2,6 +2,37 @@ import React from "react";
 
 function UpdateModal() {
     return (
-        <></>
+       <div className="modal fade" id="editModal" tabindex="-1">
+        <div className="modal-dialog">
+            <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title">학생 정보 수정</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                <div className="modal-body">
+                    <div className="mb-3">
+                        <label className="form-label">ID (필수!)</label>
+                        <input type="text" className="form-control" id="edit_id"/>
+                    </div>
+                    <div class="mb-3">
+                        <label className="form-label">Name</label>
+                        <input type="text" className="form-control" id="edit_name"/>
+                    </div>
+                    <div class="mb-3">
+                        <label className="form-label">Age</label>
+                        <input type="number" className="form-control" id="edit_age"/>
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Score</label>
+                        <input type="number" className="form-control" id="edit_score"/>
+                    </div>
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">닫기</button>  
+                    <button type="button" className="btn btn-primary" id="btn_save_edit">수정하기</button>
+                </div>
+            </div>
+        </div>
+    </div>
     );
 }
